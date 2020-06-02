783ecc29cdfb98de4b479ebe3a2fec28b690f373
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-01 21:46:55 -0400

    Scripts for App.svelte

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,4 @@
-$(document).on("scroll", () => {
-	if ($(document).scrollTop() > 15) {
-		$("header").addClass("headerScrolled");
-	} else {
-		$("header").removeClass("headerScrolled");
-	}
-});
+
 
 $(document).ready(() => {
 	$("body").css("clip-path", "circle(1000% at 50% 50%)");
