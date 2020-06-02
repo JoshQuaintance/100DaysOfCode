01e47d5f508478436f7e2a8da85e6e66dfebb4b0
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-01 21:48:19 -0400

    Added loading animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
   import Tasks from "./Tasks/Tasks.svelte";
+  import { loader } from "./scripts";
   import { projects } from "./taskData";
 
   let dataLength = Object.keys(projects);
+  let lastOfDataList = dataLength.length;
+  
 
 
   function countUpTo100(countFrom) {
@@ -14,11 +17,26 @@
     var secondsInADay = 60 * 60 * 1000 * 24;
 
     let days = Math.floor((timeDifference / secondsInADay) * 1);
-    return days
+    return days;
     /*
     let idEl = document.getElementById(id);
     idEl.getElementsByClassName("days")[0].innerHTML = days;*/
   }
+
+  let showProjects = false;
+  
+  let moreProjects = () => {(showProjects = !showProjects)
+  console.log(showProjects)
+  };
+
+  let homeBtn = () => {
+    if(showProjects == true) {
+      loader(moreProjects);
+    } else if(showProjects == false) return;
+  }
+
+
+  
 </script>
 
 <style type="text/scss">
@@ -44,28 +62,69 @@
   }
 </style>
 
+<div class="loader"/>
+
+<header>
+
+  <a href="https://google.com">
+    <img src="./images/Logo.svg" alt="Logo" id="mainLogo" />
+  </a>
+
+  <div class="headerLogos">
+
+    <a href="#top" id="home">
+      <img src="./images/home.svg" alt="Home" on:click={homeBtn} id="homeBtn"/>
+    </a>
+
+    <a href="https://github.com/joshuapelealu" target="_blank" id="githubLink">
+      <img src="./images/github.svg" alt="Github Mark" id="githubMark" />
+    </a>
+
+    <a href="https://twitter.com/bigbrainfes" target="_blank" id="twitterLink">
+      <img src="./images/twitter.svg" alt="Twitter" id="twitterLogo" />
+    </a>
+
+  </div>
+</header>
+
 <!-- Start of HTML -->
-<div class="shell">
+<div class="shell" >
 
   <main>
     <h1>Why hello there, Catalactics here...</h1>
-    <p>Welcome to my <a href="https://100daysofcode.com" class="fancyLink" target="_blank">#100DaysOfCode</a> Challenge Website Today is day <span id="dayCount">{countUpTo100('May 20, 2020 12:00:00')}</span>
+    <p>
+      Welcome to my
+      <a href="https://100daysofcode.com" class="fancyLink" target="_blank">
+        #100DaysOfCode
+      </a>
+      Challenge Website Today is day
+      <span id="dayCount">{countUpTo100('May 20, 2020 00:00:00')}</span>
     </p>
+    
+    {#if showProjects == false}
+      <div class="link-containers">
+        <div class="projects-cont">
+          <span>Projects</span>
 
-  <!--
-    <div class="link-containers">
-      <span>Projects</span>
-      <span>Progress</span>
-    </div>-->
+        </div>
 
-    <div class="tasks-container">
+        <div class="dev-log">
+          <span>Dev Log</span>
+          <h6>Latest From The Dev Logs</h6>
+          <Tasks {...projects[`Day${lastOfDataList}`]} style={'width: 80%;'} />
 
-      {#each dataLength as _, i}
-        <Tasks {...projects[`${_}`]} />
-      {/each}
+          <p on:click={() => {loader(moreProjects); console.log(showProjects)}}>View More</p>
+        </div>
+      </div>
+    {:else}
+      <div class="tasks-container">
 
-    </div>
+        {#each dataLength as _, i}
+          <Tasks {...projects[`${_}`]} />
+        {/each}
 
+      </div>
+    {/if}
   </main>
 
   <footer>
