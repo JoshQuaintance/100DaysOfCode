48be122aae3abc22669a125e37fc3a86f21c1ae6
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-04 21:57:12 -0400

    New component - Projects.svelte

---
+++ src/projects/projects.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let customStyle;
+</script>
+
+<style type="text/scss">
+  @import './projects.scss';
+</style>
+
+<!--Start code here-->
+<div class="projects-container" style={customStyle}>
+
+  <a href="https://joshuapelealu.github.io/WebDesign/" class="project-link" target="_blank">
+
+    <div class="img-cont">
+
+        <img
+        src="./project-images/codesx3.png"
+        alt="Codes Codes Codes Project Screenshot" />
+
+    </div>
+
+  </a>
+
+</div>
