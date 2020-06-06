6e0dffd5557b12796c4baf5dcbc1dfe17096c135
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-06 09:58:30 -0400

    Fixes

--- src/scripts.js
+++ src/scripts.js
@@ -3,13 +3,13 @@ import $ from "jquery";
 export let loader = (funcCallBack) => {
 	$(".loader").css({
 		"z-index" : "1000",
-		opacity : "1"
+		opacity   : "1"
 	});
 	$("header").css("opacity", "0");
 	$("body").css("overflow", "hidden");
 	$(".shell").css({
-		transition : "clip-path .5s ease",
-		"clip-path"  : "circle(0% at 50% 50%)"
+		transition  : "clip-path .5s ease",
+		"clip-path" : "circle(0% at 50% 50%)"
 	});
 
 	setTimeout(() => {
@@ -19,7 +19,7 @@ export let loader = (funcCallBack) => {
 	setTimeout(() => {
 		$(".loader").css({
 			"z-index" : "-1",
-			opacity : "1"
+			opacity   : "0"
 		});
 		$(".shell").css("clip-path", "circle(1000% at 50% 50%)");
 		$("header").css("opacity", "1");
