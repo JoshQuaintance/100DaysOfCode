8832ee9176fe355374df1c0690dd2bfa74c299a0
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-07 21:46:07 -0400

    Added projectData for projects

---
+++ src/componentData/projectsData.js
@@ -0,0 +1,10 @@
+export let projectsData = {
+	project1 : {
+		title       : "Codes Codes Codes!",
+		description : "A school web design project that talks about how awesome codes are",
+		link        : "https://joshuapelealu.github.io/WebDesign",
+		imgSrc      : "./project-images/codesx3.PNG",
+		imgAlt      : "Codes Codes Codes Web Design Project",
+		tags        : [ "HTML", "Svelte", "SCSS", "JS" ]
+	}
+};
