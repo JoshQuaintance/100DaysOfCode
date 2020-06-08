b6191dfe18b03a731ab09408abb0ad0badd8fee0
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-07 21:46:41 -0400

    Moved to Components || More functionality

---
+++ src/Components/projects/projects.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Tags from "../Tags/tags.svelte";
+
+  export let customStyle, title, link, imgSrc, imgAlt, description, tags;
+
+</script>
+
+<style type="text/scss">
+  @import "./projects.scss";
+</style>
+
+<!--Start code here-->
+<div class="projects-container" style={customStyle}>
+
+  <a href={link} class="project-link" target="_blank">
+
+    <div class="img-cont">
+
+      <img src={imgSrc} alt={imgAlt} />
+
+    </div>
+
+  </a>
+
+  <h1 class="project-title">{title}</h1>
+
+  <p class="project-description">{description}</p>
+
+  <Tags {tags}/>
+
+</div>
