769c15a538105f9d8e3389b557efe5c2fb314d86
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-07 21:46:59 -0400

    Moved to Components || More functionality

---
+++ src/Components/Tags/tags.svelte
@@ -0,0 +1,58 @@
+<script>
+  let showTags = true;
+  export let HTML = false,
+    SCSS = false,
+    JS = false,
+    jQuery = false,
+    Svelte = false,
+    tags;
+
+    // Shorten Custom indexOf function
+      let checkProjectFor = (tag) => tags.indexOf(tag) >= 0; 
+    // ----------
+      
+    // Function checking for tags
+      let checkTags = () => {
+        if(checkProjectFor("HTML")) HTML = true;
+        if(checkProjectFor("SCSS")) SCSS = true;
+        if(checkProjectFor("JS")) JS = true;
+        if(checkProjectFor("jQuery")) jQuery = true;
+        if(checkProjectFor("Svelte")) Svelte = true;
+      }
+      checkTags();
+    // ----------
+
+    
+</script>
+
+<style type="text/scss">
+  @import "tags.scss";
+</style>
+
+{#if showTags}
+
+  <div class="tags-cont">
+
+    {#if HTML}
+      <p class="left start html">HTML</p>
+    {/if}
+    <!--  -->
+    {#if SCSS}
+      <p class="right scss">SCSS</p>
+    {/if}
+
+    {#if JS}
+      <p class="left JS">JavaScript</p>
+    {/if}
+
+    {#if jQuery}
+      <p class="right JQuery">jQuery</p>
+    {/if}
+
+    {#if Svelte}
+      <p class="left Svelte">Svelte</p>
+    {/if}
+
+      
+  </div>
+{/if}
