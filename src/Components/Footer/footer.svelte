08e280bcd421f68c9e40ad5598184d77f2066791
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-09 21:49:31 -0400

    Footer Component

---
+++ src/Components/Footer/footer.svelte
@@ -0,0 +1,29 @@
+ <script>
+ 
+ </script>
+ 
+<style type="text/scss">
+@import 'footer.scss';
+</style>
+
+ <footer>
+    <p id="madeWithLove">
+      Using
+      <a href="https://svelte.dev" target="_blank">
+
+        <img
+          src="https://res.cloudinary.com/practicaldev/image/fetch/s--be3eR7Dm--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://nunomalex.me/svelte-logo.png"
+          alt="Svelte"
+          id="svelte-logo" />
+      </a>
+      and
+      <a href="https://code.visualstudio.com">
+        <img
+          src="https://cdn.worldvectorlogo.com/logos/visual-studio-code-1.svg"
+          alt=""
+          id="vscode-logo" />
+        VS Code
+      </a>
+      . Made with 💙 by Joshua P.
+    </p>
+  </footer>
